30463eec0ffd3dcb75bc30f910b75916ab92ce64
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-08 12:49:33 +0200

    navbar and cutomize pages

--- app/views.py
+++ app/views.py
@@ -1,5 +1,23 @@
 from app import app
 
+from flask import render_template
+
 @app.route("/")
-def index():
-    return "hello"
+def home():
+    return render_template("public/home.html")
+
+@app.route("/about")
+def about():
+    return render_template("public/about.html")  
+
+@app.route("/contact")
+def contact():
+    return render_template("public/contact.html")    
+
+@app.route("/signup")
+def signup():
+    return render_template("public/signup.html")  
+
+@app.route("/login")
+def login():
+    return render_template("public/login.html")             
