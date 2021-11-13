bb9c8313de41738edccf4919c61c7ae7993b4647
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-14 01:14:34 +0200

    config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,17 @@
-import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from dotenv import load_dotenv
-load_dotenv()
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SECURITY_PASSWORD_SALT']=os.getenv('SECURITY_PASSWORD_SALT')
-
-#DataBase setup
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DEV_DATABASE_URL')
 db=SQLAlchemy(app)
-
-#Email Setup
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'attirehman388@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
--- config.py
+++ config.py
@@ -7,12 +7,23 @@ load_dotenv()
 # dotenv_path = os.path.join(APP_ROOT, '.env')
 # load_dotenv(dotenv_path)
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
-#load_dotenv(os.path.join(basedir, '.env'))
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     DEBUG=False
     TESTING=False
+    SECRET_KEY= os.getenv('SECRET_KEY')
+    SECURITY_PASSWORD_SALT=os.getenv('SECURITY_PASSWORD_SALT')    
+
+    SQLALCHEMY_TRACK_MODIFICATIONS= False
+    SQLALCHEMY_DATABASE_URI=os.getenv('DEV_DATABASE_URL')
+    #Email Setup
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS= True
+    MAIL_USERNAME= 'attirehman388@gmail.com'
+    MAIL_PASSWORD= os.getenv('EMAIL_PASS')
     
   
 
